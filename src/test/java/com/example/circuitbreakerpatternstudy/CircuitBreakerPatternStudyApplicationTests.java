d973bae4006570917a48317c983f01574b416b35
KimJuHyun <rlawngus0910@naver.com>
2024-08-10 21:30:45 +0900

    init commit

---
+++ src/test/java/com/example/circuitbreakerpatternstudy/CircuitBreakerPatternStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circuitbreakerpatternstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerPatternStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
